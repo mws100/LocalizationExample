95d87a1b34655bfd4b8ba29236709e5904057363
马文帅 <mawenshuai100.com>
2016-06-21 17:19:47 +0800

    语言本地化，可修改appstore上的显示语言

---
+++ LocalizationExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationDemo
+//
+//  Created by 马文帅 on 16/6/21.
+//  Copyright © 2016年 ekeguan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizationExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationDemo
+//
+//  Created by 马文帅 on 16/6/21.
+//  Copyright © 2016年 ekeguan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
